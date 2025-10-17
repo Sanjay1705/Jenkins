51ccafd52278e5a5e34fc6d5b57e6427d5860920
Sanjay1705 <sanjaymehra1715@gmail.com>
2025-10-17 20:43:12 +0530

    Add

--- Practical-02/test_app.py
+++ Practical-02/test_app.py
@@ -1,5 +1,6 @@
 import pytest
-from app import app 
+from unittest.mock import patch
+from app import app
 
 @pytest.fixture
 def client():
@@ -8,12 +9,18 @@ def client():
 def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.data == b"OK"  
+    assert response.data == b"OK"
 
-def test_counter_increment(client):
+@patch("app.r")  
+def test_counter_increment(mock_redis, client):
+
+    mock_redis.set.return_value = True
 
     response1 = client.get("/counter")
     val1 = int(response1.data)
     response2 = client.get("/counter")
     val2 = int(response2.data)
+    
     assert val2 == val1 + 1
+    
+    assert mock_redis.set.call_count == 2
